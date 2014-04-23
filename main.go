5c6e75d00c8ff73029bbbfc97e9916f19ee55f98
王家雷 <wangjialei@pinidea.com>
2014-04-23 12:32:14 +0800

    add -o flag and auto find apk feature

--- main.go
+++ main.go
@@ -5,14 +5,15 @@ import "github.com/QQ1378028155/go-pac/logger"
 import "github.com/QQ1378028155/go-pac/worker"
 
 var (
-	file    = flag.String("f", "", "configuration file to input.")
+	infile  = flag.String("f", "", "configuration file to input.")
 	verbose = flag.Bool("v", true, "give out some output on the screen")
+	outfile = flag.String("o", "", "packaged file to output")
 )
 
 func main() {
 	flag.Parse()
 	logger.Verbose = *verbose
-	err := worker.Run(*file)
+	err := worker.Run(*infile, *outfile)
 	if err != nil {
 		logger.Debug(err.Error())
 	}
--- worker/worker.go
+++ worker/worker.go
@@ -10,8 +10,16 @@ import "github.com/QQ1378028155/go-pac/cmd"
 import "errors"
 import "strings"
 import "os/user"
+import "regexp"
 
 var antPropertyTemplate string
+
+var (
+	apkReg, _          = regexp.Compile("\\.apk$") // regular expression to find the file
+	unsignedapkReg, _  = regexp.Compile("-unsigned\\.apk$")
+	unalignedapkReg, _ = regexp.Compile("-unaligned\\.apk$")
+	workdir, _         = os.Getwd() //record the base info
+)
 
 func init() {
 	antPropertyTemplate = "key.store=%s\n" +
@@ -20,7 +28,7 @@ func init() {
 		"key.alias.password=%s"
 }
 
-func Run(filename string) (err error) {
+func Run(filename, outfile string) (err error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return
@@ -44,26 +52,30 @@ func Run(filename string) (err error) {
 		return
 	}
 	if config.Android != nil {
-		return compileAndroid(config.Android)
+		return compileAndroid(config.Android, outfile)
 	}
 	return
 }
 
-func compileAndroid(config *conf.AndroidConfig) (err error) {
+func compileAndroid(config *conf.AndroidConfig, outfile string) (err error) {
+	var file *os.File
+
+	// remove the old build
 	err = cmd.SyncCmd("ant", []string{"clean", "-Dsdk.dir=/usr/lib/android/sdk"})
 	if err != nil {
 		return
 	}
 	var sign bool
+	// set sign true if all infomation for signer is collected
 	if config.Store != nil && config.StorePassword != nil && config.Alias != nil && config.AliasPassword != nil {
 		sign = true
 	}
 
+	// generate ant.properties for ant to sign the apk while compliling and packing.
+
 	if sign == true {
 		str := fmt.Sprintf(antPropertyTemplate, *config.Store, *config.StorePassword, *config.Alias, *config.AliasPassword)
-		var file *os.File
 		file, err = os.OpenFile("ant.properties", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
-		defer file.Close()
 		if err != nil {
 			return
 		}
@@ -72,21 +84,82 @@ func compileAndroid(config *conf.AndroidConfig) (err error) {
 			return
 		}
 	}
-
+	// use ant release to build and pack the project.
 	err = cmd.SyncCmd("ant", []string{"release", "-Dsdk.dir=/usr/lib/android/sdk"})
 	if err != nil {
 		return
 	}
+
+	// remove the ant.properties will avoid the git conflict
 	if sign == true {
 		err = os.Remove("ant.properties")
 		if err != nil {
 			return
 		}
 	}
+
+	// Anaylse the build directory and found .apk file.
+	// If sign is set false. find -unsigned.apk
+	// if sign is set true. find the .apk without -unsigned and -unaligned
+	buildDir, err := os.Open("./bin")
+	if err != nil {
+		return err
+	}
+	fileInfos, err := buildDir.Readdir(0)
+	if err != nil {
+		return err
+	}
+
+	var targetApkPath string
+	for i := 0; i < len(fileInfos); i++ {
+		if fileInfos[i].IsDir() == true {
+			continue
+		}
+		filename := fileInfos[i].Name()
+		b0 := apkReg.Match([]byte(filename))
+		b1 := unalignedapkReg.Match([]byte(filename))
+		if b0 == false || b1 == true {
+			continue
+		}
+		b2 := unsignedapkReg.Match([]byte(filename))
+
+		if sign == b2 {
+			continue
+		} else {
+			var wd string
+			wd, err = os.Getwd()
+			if err != nil {
+				return
+			}
+			targetApkPath = wd + "/bin/" + filename
+			break
+		}
+	}
+
+	if targetApkPath == "" {
+		err = errors.New("No apk found")
+	}
+	logger.Debug("Find " + targetApkPath)
+	// targetApkPath records the absolute path of target apk.
+	// change the working dirctory last time and copy the apk file
+
+	logger.Debug("Enter" + workdir)
+	err = os.Chdir(workdir) //recover the work directory caused by fetchFromRemote
+	if err != nil {
+		return
+	}
+	logger.Debug("copy " + targetApkPath + " to " + outfile)
+	cmd.SyncCmd("cp", []string{"-R", targetApkPath, outfile})
+
+	defer func() {
+		file.Close()
+		buildDir.Close()
+	}()
 	return
 }
 
 // clone or pull the repo from remote
+// ATTENTION!!! : The working directory changed after the function is called
 func fetchFromRemote(repo string) (err error) {
 	u, err := user.Current()
 	if err != nil {
